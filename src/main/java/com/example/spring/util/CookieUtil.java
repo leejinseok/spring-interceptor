71a41abbeaae2e2c5bb582ffeb332c4ac03bc24d
이진석 <sonaky47@gmai.com>
2019-11-14 12:03:56 +0900

    remove unused import

--- src/main/java/com/example/spring/util/CookieUtil.java
+++ src/main/java/com/example/spring/util/CookieUtil.java
@@ -2,7 +2,6 @@ package com.example.spring.util;
 
 
 import com.example.spring.config.AppProperties;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
